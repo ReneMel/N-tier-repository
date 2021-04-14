786cfc7a968e0a4179ab1dcbafb64315fd3d6737
René Meléndez <35284243+ReneMel@users.noreply.github.com>
2021-04-14 14:22:01 -0600

    [fixed] Stored Procedure Tarea01

--- Tarea01/db/InsertsandProcedures.sql
+++ Tarea01/db/InsertsandProcedures.sql
@@ -13,9 +13,10 @@ insert into  rol(nombre,descripcion) values ('usuario', 'pos es un usuario');
 --listar 
 create or alter proc listar
 as 
-select p.idpersona,r.nombre as rol , p.nombre, p.apellido, p.edad, p.telefono 
+select p.idpersona as ID , p.apellido as APELLIDO, p.nombre as NOMBRE, p.edad as EDAD, p.telefono as TELEFONO, r.nombre as ROL  
 from persona p left join rol r 
-on r.idrol=p.idrol;
+on r.idrol=p.idrol
+order by p.idpersona desc;
 go; 
 
 
