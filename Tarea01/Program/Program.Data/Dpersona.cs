ff379d5b25bf1305a7fc54a4d0e449c4dbe073f4
René Meléndez <35284243+ReneMel@users.noreply.github.com>
2021-04-13 23:12:17 -0600

    [add]  Entities and DPersona created

---
+++ Tarea01/Program/Program.Data/Dpersona.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Program.Entidades;
+using System.Data.SqlClient;
+
+namespace Program.Data
+{
+    public class Dpersona
+    {
+        public DataTable Listar() {
+
+            SqlDataReader res;
+            DataTable tabla = new DataTable();
+            SqlConnection con = new SqlConnection();
+            try
+            {
+                con = Conexion.getInstancia().CrearConexion();
+                SqlCommand cmd = new SqlCommand("listar", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                res = cmd.ExecuteReader();
+                tabla.Load(res);
+                return tabla;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally {
+                if (con.State == ConnectionState.Open) {
+                    con.Close();
+                }
+            
+            }
+
+        }
+
+        public String Insertar(Persona Obj) { 
+
+        }
+    }
+}
---
+++ Tarea01/Program/Program.Entidades/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program.Entidades
+{
+    public class Persona
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public int edad { get; set; }
+        public string tel { get; set; }
+    }
+
+
+}
